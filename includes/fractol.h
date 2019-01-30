3e495d1669183bfe515510e97510aec3c7595fbc
Bradford Falmer nightprowler <texetomg@gmail.com>
2019-01-30 20:05:19 +0300

    win init

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:24:42 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/01/30 12:50:41 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/01/30 18:46:20 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,34 @@
 # include "../minilibx_macos/mlx.h"
 # include "../libft/libft.h"
 # include <math.h>
+# define WDTH	1200
+# define HGHT	900
 
-typedef struct  s_mlx_win
+typedef struct  s_img
 {
-	void    *mlx;
-	void    *win;
-}               t_mlx_win;
+	void		*img_ptr;
+	void		*addr_ptr;
+	int			size_line;
+	int			endian;
+	int			bpp;
+}               t_img;
+
+typedef	struct 	s_mlx_win
+{
+	void    	*mlx_ptr;
+	void    	*win_ptr;	
+}				t_mlx_win;
+
+
+typedef	struct 	s_fractal
+{
+	int		c;
+	int		z0;
+	int		z1;
+	int		x;
+	int		y;	
+}				t_fractal;
+
 
 void            arg_error(void);
 void            malloc_error(void);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:38:33 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/01/30 12:57:48 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/01/30 20:04:52 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,34 @@ void    check_ac_av(int ac, char *av)
 		arg_error();
 }
 
+void	drow_mandelbrot(t_mlx_win mlx_win, t_img img)
+{
+	t_fractal	*frac;
+
+	if (!(frac = (t_fractal*)malloc(sizeof(t_fractal))))
+		malloc_error();
+		//
+}
+
 void    window_init(char *av)
 {
-	t_mlx_win   *mlx_win;
+	t_img   	*img;
+	t_mlx_win	*mlx_win;
 
+	if (!(img = (t_img*)malloc(sizeof(t_img))))
+		malloc_error();
 	if (!(mlx_win = (t_mlx_win*)malloc(sizeof(t_mlx_win))))
 		malloc_error();
-	if (!(mlx_win->mlx = mlx_init()))
-		 mlx_win_error();
-	if (!(mlx_win->win = mlx_new_window(mlx_win->mlx, 1200, 900, av)))
-		 mlx_win_error();
-	mlx_loop(mlx_win->mlx);
+	if (!(mlx_win->mlx_ptr = mlx_init()))
+		mlx_win_error();
+	if (!(mlx_win->win_ptr = mlx_new_window(mlx_win->mlx_ptr, WDTH, HGHT, av)))
+		mlx_win_error();
+	img->img_ptr = mlx_new_image(mlx_win->mlx_ptr, WDTH, HGHT);
+	img->addr_ptr = mlx_get_data_addr(img->img_ptr,
+					&(img->bpp), &(img->size_line), &(img->endian));
+	img->bpp = img->bpp / 8;
+	draw_mandelbrot(mlx_win, img);
+	mlx_loop(mlx_win->mlx_ptr);
 }
 
 int     main(int ac, char **av)
