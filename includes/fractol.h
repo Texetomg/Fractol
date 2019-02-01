254c774a30d7c140ce8c4aad143cfc4b5d130f8b
Bradford Falmer nightprowler <texetomg@gmail.com>
2019-02-01 15:39:14 +0300

    add draw mandelbrot

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:24:42 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/01/30 18:46:20 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/02/01 15:04:59 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 # include "../minilibx_macos/mlx.h"
 # include "../libft/libft.h"
 # include <math.h>
-# define WDTH	1200
-# define HGHT	900
+# define WDTH	1600
+# define HGHT	1200
 
 typedef struct  s_img
 {
@@ -36,11 +36,15 @@ typedef	struct 	s_mlx_win
 
 typedef	struct 	s_fractal
 {
-	int		c;
-	int		z0;
-	int		z1;
-	int		x;
-	int		y;	
+	int		color;
+	int		x0;
+	int		y0;
+	double	real;
+	double	imag;
+	double	c_real;
+	double	c_imag;
+	double	ex_real;
+	double	ex_imag;
 }				t_fractal;
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 14:38:33 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/01/30 20:04:52 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/02/01 15:38:24 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,50 @@ void    check_ac_av(int ac, char *av)
 		arg_error();
 }
 
-void	drow_mandelbrot(t_mlx_win mlx_win, t_img img)
+int	mandelbrot(t_fractal *fractal, int x, int y)
 {
-	t_fractal	*frac;
+	int			i;
 
-	if (!(frac = (t_fractal*)malloc(sizeof(t_fractal))))
+	i = 0;
+	fractal->color = 0xFFFFFF;
+	fractal->c_real = ((double)(x - fractal->x0)) / (WDTH / 5);
+	fractal->c_imag = ((double)(fractal->y0 - y)) / (HGHT / 5);
+	fractal->real = 0;
+	fractal->imag = 0;
+	while (i < 250 && (fractal->real * fractal->real + fractal->imag * fractal->imag) <= 4)
+	{
+		fractal->ex_real = fractal->real * fractal->real - fractal->imag * fractal->imag + fractal->c_real;
+		fractal->ex_imag = 2 * fractal->real * fractal->imag + fractal->c_imag;
+		fractal->real = fractal->ex_real;
+		fractal->imag = fractal->ex_imag;
+		i++;
+		fractal->color = fractal->color - 470 * 470;
+	}
+	return (fractal->color);
+}
+
+void	drow_img(t_mlx_win *mlx_win, t_img *img)
+{
+	int			x;
+	int			y;
+	t_fractal	*fractal;
+
+	y = 0;
+	if (!(fractal = (t_fractal*)malloc(sizeof(t_fractal))))
 		malloc_error();
-		//
+	fractal->x0 = WDTH / 2;
+	fractal->y0 = HGHT / 2;
+	while(y < HGHT)
+	{
+		x = 0;
+		while(x < WDTH)
+		{
+			*((int*)(img->addr_ptr) + x + y * WDTH) = mandelbrot(fractal, x, y);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(mlx_win->mlx_ptr, mlx_win->win_ptr, img->img_ptr, 0, 0);
 }
 
 void    window_init(char *av)
@@ -46,7 +83,7 @@ void    window_init(char *av)
 	img->addr_ptr = mlx_get_data_addr(img->img_ptr,
 					&(img->bpp), &(img->size_line), &(img->endian));
 	img->bpp = img->bpp / 8;
-	draw_mandelbrot(mlx_win, img);
+	drow_img(mlx_win, img);
 	mlx_loop(mlx_win->mlx_ptr);
 }
 
