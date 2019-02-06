fa5818095f8583c18ed8494801b3dacb7f928c07
Bradford Falmer nightprowler <texetomg@gmail.com>
2019-02-06 16:36:51 +0300

    trasfer some string in another func

--- src/key_hook.c
+++ src/key_hook.c
@@ -6,20 +6,20 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 20:15:35 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/02/06 16:30:32 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/02/06 16:35:22 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int    key_press(int key)
+int		key_press(int key)
 {
 	if (key == 53)
 		exit(1);
 	return(0);
 }
 
-void		mouse_scroll(int key, int x, int y, t_fractal *fractal)
+void	mouse_scroll(int key, int x, int y, t_fractal *fractal)
 {
 	double	x0;
 	double	y0;
@@ -32,7 +32,6 @@ void		mouse_scroll(int key, int x, int y, t_fractal *fractal)
 	y1 = (fractal->y0 - fractal->y1) * ((double)(y) / 10000);
 	if (key == 5)
 	{
-		
 		fractal->x0 = fractal->x0 + x0;
 		fractal->x1 = fractal->x1 - x1;
 		fractal->y0 = fractal->y0 - y0;
@@ -45,14 +44,14 @@ void		mouse_scroll(int key, int x, int y, t_fractal *fractal)
 		fractal->y0 = fractal->y0 + y0;
 		fractal->y1 = fractal->y1 - y1;
 	}
-	mlx_clear_window(fractal->mlx_ptr, fractal->win_ptr);
-	drow_img(fractal);
 }
 
 int		mouse_hook(int key, int x, int y, t_fractal *fractal)
 {
 	if (key == 5 || key == 4)
-		mouse_scroll(key, x, y, fractal);	
+		mouse_scroll(key, x, y, fractal);
+	mlx_clear_window(fractal->mlx_ptr, fractal->win_ptr);
+	drow_img(fractal);
 	return(0);
 }
 
